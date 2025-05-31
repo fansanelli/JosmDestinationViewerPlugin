c2ccb49ef7388e7271e37fbed0860eea57b7f262
Francesco Ansanelli <francians@gmail.com>
2025-05-31 18:16:03 +0200

    git commit -m "first commit"

---
+++ README.md
@@ -0,0 +1,12 @@
+This project depends on: https://github.com/fansanelli/OsmDestinationViewer
+
+Copy "josm-latest.jar" in the "lib" folder.
+
+To build you need Maven and run: 
+
+`mvn clean package`
+
+In the "target" folder you will find the jar with all the dependencies.
+
+Copy it in ~/.local/share/JOSM/plugins
+
---
+++ src/main/java/org/openstreetmap/josm/plugins/josmdestinationviewer/JosmDestinationViewerPlugin.java
@@ -0,0 +1,71 @@
+package org.openstreetmap.josm.plugins.josmdestinationviewer;
+
+import java.awt.BorderLayout;
+import java.io.StringReader;
+import java.util.Map;
+import java.util.Set;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.batik.swing.JSVGCanvas;
+import org.openstreetmap.josm.data.osm.DataSelectionListener;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.data.osm.event.SelectionEventManager;
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+import org.w3c.dom.Document;
+
+import dev.pengunaria.osmdestinationviewer.OsmDestinationViewer;
+
+public class JosmDestinationViewerPlugin extends Plugin implements DataSelectionListener {
+	public JosmDestinationViewerPlugin(PluginInformation info) {
+		super(info);
+		// Registra il listener per la selezione sul dataset attivo
+		SelectionEventManager.getInstance().addSelectionListener(this);
+	}
+
+	public void selectionChanged(SelectionChangeEvent event) {
+		Set<OsmPrimitive> selection = event.getSelection();
+		if (selection == null || selection.size() != 1) {
+			return;
+		}
+		OsmPrimitive primitive = selection.iterator().next();
+		if (primitive instanceof Way || primitive instanceof Node) {
+			Map<String, String> tags = primitive.getKeys();
+			if (tags.containsKey("destination")) {
+				try {
+					// TODO Ask JOSM the country code
+					String svgContent = new OsmDestinationViewer(tags, "IT").getSvg();
+					showSvgPanel(svgContent);
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(MainApplication.getMainFrame(), "Errore " + e.getMessage());
+				}
+			}
+		}
+	}
+
+	private static void showSvgPanel(String svgContent) {
+		JSVGCanvas svgCanvas = new JSVGCanvas();
+		svgCanvas.setDocumentState(JSVGCanvas.ALWAYS_DYNAMIC);
+		try {
+			svgCanvas.setURI(null);
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setNamespaceAware(true);
+			Document doc = factory.newDocumentBuilder()
+					.parse(new org.xml.sax.InputSource(new StringReader(svgContent)));
+			svgCanvas.setDocument(doc);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(MainApplication.getMainFrame(),
+					"Errore nel rendering SVG: " + e.getMessage());
+			return;
+		}
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(svgCanvas, BorderLayout.CENTER);
+		JOptionPane.showMessageDialog(MainApplication.getMainFrame(), panel, "SVG Viewer", JOptionPane.PLAIN_MESSAGE);
+	}
+}
