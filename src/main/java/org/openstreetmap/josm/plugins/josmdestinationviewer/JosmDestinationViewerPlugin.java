e431ac1e41c2b4ae5c792ab8b9f1bfb5e1443428
Francesco Ansanelli <francians@gmail.com>
2025-06-07 17:10:35 +0200

    Creato un tasto esplicito per vedere i cartelli

--- src/main/java/org/openstreetmap/josm/plugins/josmdestinationviewer/JosmDestinationViewerPlugin.java
+++ src/main/java/org/openstreetmap/josm/plugins/josmdestinationviewer/JosmDestinationViewerPlugin.java
@@ -18,72 +18,101 @@
 
 package org.openstreetmap.josm.plugins.josmdestinationviewer;
 
-import java.awt.BorderLayout;
-import java.io.StringReader;
-import java.util.Map;
-import java.util.Set;
+import static org.openstreetmap.josm.tools.I18n.tr;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.io.StringReader;
+import java.util.Collection;
+import java.util.Map;
+
+import javax.swing.JMenu;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.batik.swing.JSVGCanvas;
+import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.data.osm.DataSelectionListener;
-import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
-import org.openstreetmap.josm.data.osm.Way;
-import org.openstreetmap.josm.data.osm.event.SelectionEventManager;
 import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.MainMenu;
+import org.openstreetmap.josm.gui.Notification;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 import org.w3c.dom.Document;
 
 import main.java.dev.pengunaria.osmdestinationviewer.OsmDestinationViewer;
 
-public class JosmDestinationViewerPlugin extends Plugin implements DataSelectionListener {
+public class JosmDestinationViewerPlugin extends Plugin {
 	public JosmDestinationViewerPlugin(PluginInformation info) {
 		super(info);
-		// Registra il listener per la selezione sul dataset attivo
-		SelectionEventManager.getInstance().addSelectionListener(this);
+
+		JMenu jToolmenu = MainApplication.getMenu().toolsMenu;
+		jToolmenu.addSeparator();
+		MainMenu.add(jToolmenu, new MainAction("View destination"));
 	}
 
-	public void selectionChanged(SelectionChangeEvent event) {
-		Set<OsmPrimitive> selection = event.getSelection();
-		if (selection == null || selection.size() != 1) {
-			return;
+	private static class MainAction extends JosmAction implements DataSelectionListener {
+		private static final long serialVersionUID = -2357348195203211773L;
+
+		public MainAction(String text) {
+			super(tr(text), null, tr("display the destination tag as a road sign"), null, true);
 		}
-		OsmPrimitive primitive = selection.iterator().next();
-		if (primitive instanceof Way || primitive instanceof Node) {
+
+		@Override
+		public void actionPerformed(ActionEvent actionEvent) {
+			if (!isEnabled())
+				return;
+
+			Collection<OsmPrimitive> selection = getLayerManager().getEditDataSet().getAllSelected();
+			if (selection.isEmpty() || selection.size() != 1) {
+				new Notification(tr("You have to select exactly one element.")).setIcon(JOptionPane.WARNING_MESSAGE)
+						.setDuration(Notification.TIME_SHORT).show();
+				return;
+			}
+
+			OsmPrimitive primitive = selection.iterator().next();
 			Map<String, String> tags = primitive.getKeys();
-			if (tags.containsKey("destination")) {
-				try {
-					// TODO Ask JOSM the country code
-					String svgContent = new OsmDestinationViewer(tags, "IT").getSvg();
-					showSvgPanel(svgContent);
-				} catch (Exception e) {
-					JOptionPane.showMessageDialog(MainApplication.getMainFrame(), "Errore " + e.getMessage());
-				}
+			try {
+				// TODO Ask JOSM the country code
+				String svgContent = new OsmDestinationViewer(tags, "IT").getSvg();
+				showSvgPanel(svgContent);
+			} catch (Exception e) {
+				new Notification(e.getMessage()).setIcon(JOptionPane.ERROR_MESSAGE).setDuration(Notification.TIME_LONG)
+						.show();
 			}
 		}
-	}
 
-	private static void showSvgPanel(String svgContent) {
-		JSVGCanvas svgCanvas = new JSVGCanvas();
-		svgCanvas.setDocumentState(JSVGCanvas.ALWAYS_DYNAMIC);
-		try {
-			svgCanvas.setURI(null);
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			factory.setNamespaceAware(true);
-			Document doc = factory.newDocumentBuilder()
-					.parse(new org.xml.sax.InputSource(new StringReader(svgContent)));
-			svgCanvas.setDocument(doc);
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(MainApplication.getMainFrame(),
-					"Errore nel rendering SVG: " + e.getMessage());
-			return;
+		private static void showSvgPanel(String svgContent) {
+			JSVGCanvas svgCanvas = new JSVGCanvas();
+			svgCanvas.setDocumentState(JSVGCanvas.ALWAYS_DYNAMIC);
+			try {
+				svgCanvas.setURI(null);
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				factory.setNamespaceAware(true);
+				Document doc = factory.newDocumentBuilder()
+						.parse(new org.xml.sax.InputSource(new StringReader(svgContent)));
+				svgCanvas.setDocument(doc);
+			} catch (Exception e) {
+				new Notification(e.getMessage()).setIcon(JOptionPane.ERROR_MESSAGE).setDuration(Notification.TIME_LONG)
+						.show();
+				return;
+			}
+			JPanel panel = new JPanel(new BorderLayout());
+			panel.add(svgCanvas, BorderLayout.CENTER);
+			JOptionPane.showMessageDialog(MainApplication.getMainFrame(), panel, "SVG Viewer",
+					JOptionPane.PLAIN_MESSAGE);
 		}
-		JPanel panel = new JPanel(new BorderLayout());
-		panel.add(svgCanvas, BorderLayout.CENTER);
-		JOptionPane.showMessageDialog(MainApplication.getMainFrame(), panel, "SVG Viewer", JOptionPane.PLAIN_MESSAGE);
+
+		@Override
+		public void selectionChanged(SelectionChangeEvent event) {
+			updateEnabledStateOnCurrentSelection();
+		}
+
+		@Override
+		protected void updateEnabledState(Collection<? extends OsmPrimitive> selection) {
+			updateEnabledStateOnModifiableSelection(selection);
+		}
 	}
 }
